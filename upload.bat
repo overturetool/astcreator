b5ebff45b294abaf5ddfbed6c581a48e1fca52ee
Kenneth Lausdahl <kel@iha.dk>
2013-09-06 19:51:52 +0200

    deploy

--- upload.bat
+++ upload.bat
@@ -1,13 +1,16 @@
 @echo off
+echo Deploying Maven Artifacts
+cmd /C mvn deploy
 REM http://cemerick.com/2010/08/24/hosting-maven-repos-on-github/
 cd target
 git clone -b mvn-repo git@github.com:overturetool/astcreator.git git
 cd mvn-repo
-xcopy /s/y . ..\git\release
+xcopy /s/y . ..\git\release\
 cd ..
 cd git
 git add *
 git commit -m "deploy"
 echo Hit enter to push
+pause
 git push
 
