bb9420b9d6ad3016a1fe2c70a78b75af9340bedc
Kenneth Lausdahl <kel@iha.dk>
2013-11-07 23:22:47 +0100

    improved speed for replaceChild

--- astCreator/src/main/resources/Node.java
+++ astCreator/src/main/resources/Node.java
@@ -74,6 +74,7 @@ public abstract class %Node% implements %INode%, Cloneable, Serializable, /*expe
 					{
 						newChild.parent(this);
 					}
+					return;
 				}else if( valueInF instanceof List)
 				{
 					@SuppressWarnings("rawtypes")
@@ -87,6 +88,7 @@ public abstract class %Node% implements %INode%, Cloneable, Serializable, /*expe
 							{
 								newChild.parent(this);
 							}
+							return;
 						}
 					}
 				}
