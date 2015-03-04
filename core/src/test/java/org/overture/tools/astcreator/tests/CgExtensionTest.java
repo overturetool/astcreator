12a9c9d12e27a390223b0bc18ce3e33fc5203e48
Luis Diogo Couto <ldc@eng.au.dk>
2015-03-04 17:53:32 +0100

    Add test for CG extension stuff
    [Issue: #3]

---
+++ core/src/test/java/org/overture/tools/astcreator/tests/CgExtensionTest.java
@@ -0,0 +1,12 @@
+package org.overture.tools.astcreator.tests;
+
+public class CgExtensionTest extends AbstractAstCreatorTestCase
+{
+
+	public void test() throws Exception
+	{
+	//	singleFileTest("cgextend/cg.astv2", "cgextend");
+		extensionFileTest("cgextend/cg.astv2", "cgextend/isacg.astv2", "cgextend", "New");
+	}
+
+}
